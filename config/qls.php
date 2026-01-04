b9997dcd41ca073aec99da2726901cd4a245f679
Berkan Kaya <berkankaya03@gmail.com>
2026-01-04 22:27:57 +0100

    Add QLS config file

---
+++ config/qls.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'base_url' => env('QLS_API_BASE_URL', 'https://api.pakketdienstqls.nl'),
+    'username' => env('QLS_API_USERNAME'),
+    'password' => env('QLS_API_PASSWORD'),
+    'company_id' => env('QLS_COMPANY_ID'),
+    'brand_id' => env('QLS_BRAND_ID'),
+    'product_combination_id' => env('QLS_PRODUCT_COMBINATION_ID'),
+    'timeout' => (int) env('QLS_API_TIMEOUT', 10),
+];
